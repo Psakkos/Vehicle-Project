f81e289f86f254a0ccc16ca06a9e6112caa41da8
Psakkos <31419207+Psakkos@users.noreply.github.com>
2017-08-29 14:47:55 -0500

    defined variables

--- TestVehicle.js
+++ TestVehicle.js
@@ -2,7 +2,9 @@ const Vehicle = require('./Vehicle.js');
 
 function main(){
     //make two vehicles and take them for a spin.
+    var hotrod = new Vehicle(8,1,20,30,0);
 
+    hotrod.drive(10);
 
 
 
--- Vehicle.js
+++ Vehicle.js
@@ -1,3 +1,11 @@
 var Vehicle= function(){
     //work in here
+    let engine= e;
+    let fuel= f;
+    let gastank=gt;
+    let averageMPG=mpg;
+    let milesDriven=md;
+
+
+
 };
