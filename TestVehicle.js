e705a70df1f41cc18b8fa224cb0ca7b1ceec4003
Psakkos <31419207+Psakkos@users.noreply.github.com>
2017-08-31 14:57:49 -0500

    defined verbs such as engine

--- TestVehicle.js
+++ TestVehicle.js
@@ -2,9 +2,12 @@ const Vehicle = require('./Vehicle.js');
 
 function main(){
     //make two vehicles and take them for a spin.
-    var hotrod = new Vehicle(8,1,20,30,0);
+    var hotrod = new Vehicle(8,20,30);
 
     hotrod.drive(10);
+    hotrod.fuelLeft();
+    hotrod.fill(0.5)
+
 
 
 
--- Vehicle.js
+++ Vehicle.js
@@ -1,11 +1,38 @@
-var Vehicle= function(){
+var Vehicle= function(e,gt,mpg){
     //work in here
     let engine= e;
-    let fuel= f;
+    let fuel= 1;
     let gastank=gt;
     let averageMPG=mpg;
-    let milesDriven=md;
+    let milesDriven=0;
 
+    //getters
+    function totalMiles(){
+        return milesDriven;
+    }
+    function fuelLeft(){
+        return fuel;
+    }
 
+    //Setter
+ function fill(percent){
+     fuel += percent;
+     if (fuel>1) {
+         fuel=1;
+     }
+ }
+ function engineEfficiency(){
+     if(engine== 4){
+         return 1;
+     }
+     if (engine == 6){
+         return 1.2;
+     }
+    if (engine== 8) {
+        return 1.5;
+    }
+ }
+
+    return{drive,fill,totalMiles,fuelLeft}
 
 };
