9530732fbfbd18a651826c362ee3029430f2143b
Psakkos <31419207+Psakkos@users.noreply.github.com>
2017-09-01 14:47:13 -0500

    Add files via upload

--- TestVehicle.js
+++ TestVehicle.js
@@ -2,14 +2,16 @@ const Vehicle = require('./Vehicle.js');
 
 function main(){
     //make two vehicles and take them for a spin.
-    var hotrod = new Vehicle(8,20,30);
-
-    hotrod.drive(10);
-    hotrod.fuelLeft();
-    hotrod.fill(0.5)
-
+    var prius = new Vehicle(8,20,30);
+    prius.drive(10);
+    prius.fuelLeft();
+    prius.fill(0.5)
 
 
+    var ferrari = new Vehicle(8,25,15)
+    ferrari.drive(20);
+    ferrari.fuelLeft();
+    ferrari.fill(0.6);
 
 
 }
--- Vehicle.js
+++ Vehicle.js
@@ -17,19 +17,36 @@ var Vehicle= function(e,gt,mpg){
     //Setter
  function fill(percent){
      fuel += percent;
-     if (fuel>1) {
+     if (fuel>1){
          fuel=1;
      }
+     else fu{
+
+     }
  }
+function drive(hour){
+    let fuelconsumed= hour*engineEfficiency();
+    if (fuelconsumed>=fuel){
+
+    }
+    else if(fuelconsumed<fuel){
+        milesDriven+=;
+    }
+}
+
+
  function engineEfficiency(){
      if(engine== 4){
          return 1;
      }
-     if (engine == 6){
+     else if (engine == 6){
          return 1.2;
      }
-    if (engine== 8) {
+    else if (engine== 8) {
         return 1.5;
+    }
+    else {
+        return null;
     }
  }
 
